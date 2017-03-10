e6cfe8c5ef20c285ad8af323f3f6280843f99d1f
synpheros <synpheros@hotmail.com>
2017-03-10 20:58:07 +0100

    mas cosas

---
+++ Assets/Generic/Chat/Sleep.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sleep : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void sleep(){
+        foreach (Transform t in this.transform) {
+            t.GetComponent<Eye> ().close ();
+        }
+    }
+}
