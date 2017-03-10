198fb602656239ea002a8db9ed0057b9fd8a2733
synpheros <synpheros@hotmail.com>
2017-03-11 00:34:50 +0100

    Dia 5 Parte 1

--- Assets/TVController.cs
+++ Assets/TVController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TVController : MonoBehaviour {
 
@@ -25,5 +26,7 @@ public class TVController : MonoBehaviour {
         whitenoise.SetActive(true);
         yield return new WaitForSeconds(1);
         futbol.SetActive(true);
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Dia5_3");
     }
 }
