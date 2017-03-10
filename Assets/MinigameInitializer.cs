c779ca1e4fbe2e322a3d8a98cc48793f99710309
Victorma Perez Colado <victorma-fire@hotmail.com>
2017-03-10 23:30:19 +0100

    Dia 1 final, dia 3 final

---
+++ Assets/MinigameInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameInitializer : EventManager {
+
+    [SerializeField]
+    BallGenerator minigameController;
+
+    bool started = false;
+    public override void ReceiveEvent(IGameEvent ev)
+    {
+        if(ev.Name == "start futbol")
+        {
+            started = true;
+        }
+    }
+
+    public override void Tick()
+    {
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        minigameController.gameObject.SetActive(started);
+
+    }
+}
