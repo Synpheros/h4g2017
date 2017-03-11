6176a7ccc47ec2c96abe0e209ef09bdf8bca3f22
Victorma Perez Colado <victorma-fire@hotmail.com>
2017-03-11 04:52:10 +0100

    Dia 6

---
+++ Assets/DialogAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogAnimator : EventManager {
+
+    public Animator animator;
+
+    private IGameEvent toFinish;
+
+    public override void ReceiveEvent(IGameEvent ev)
+    {
+        if(ev.Name == "animate")
+        {
+            toFinish = ev;
+            animator.Play((string)ev.getParameter("animation"));
+        }
+    }
+
+    public override void Tick()
+    {
+    }
+    
+
+    public void Finish()
+    {
+        Game.main.eventFinished(toFinish);
+    }
+}
